1e3e8f5bd44897b6a89e8c5b82d6c299037d5865
Elias Moura <elias.smourayt@gmail.com>
2023-11-07 15:51:29 -0300

    Utilizando props

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <Home />
+      <Home title='Olá,' description='Seja bem vinde!'/>
     </>
   )
 }
--- blog-pessoal/src/pages/home/home.tsx
+++ blog-pessoal/src/pages/home/home.tsx
@@ -1,10 +1,16 @@
 import './home.css';
 import HomeLogo from '../../assets/home/Home.jpg';
 
-function Home() {
+interface Props {
+    title: string,
+    description: string,
+}
+
+function Home({ title="Olá.", description="Seja bem vindo!" } : Props) {
     return (
         <>
             <h1 className='titulo'>Home</h1>
+            <p>{title} {description}</p>
             <img src={HomeLogo} className='img' alt="Imagem tela inicial." />
         </>
     )
