33a91857d34bd1e8e544cf0bdef2958f325bf537
Elias Moura <elias.smourayt@gmail.com>
2023-11-07 15:23:29 -0300

    Home page

--- blog-pessoal/src/pages/home/home.tsx
+++ blog-pessoal/src/pages/home/home.tsx
@@ -1,10 +1,11 @@
 import './home.css';
+import HomeLogo from '../../assets/home/Home.jpg';
 
-const Home = () => {
+function Home() {
     return (
         <>
             <h1 className='titulo'>Home</h1>
-            
+            <img src={HomeLogo} className='img' alt="Imagem tela inicial." />
         </>
     )
 }
