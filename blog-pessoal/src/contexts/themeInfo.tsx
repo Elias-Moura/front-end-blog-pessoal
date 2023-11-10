766f39c87de34e2f95a32e99774302e4be6baf9c
Elias Moura <elias.smourayt@gmail.com>
2023-11-10 17:31:52 -0300

    Context de tema

---
+++ blog-pessoal/src/contexts/themeInfo.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, createContext, useState } from "react";
+import usePersistedState from "../utils/usePersistedState";
+
+interface UserThemeType {
+  theme: string;
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const ThemeContext = createContext<UserThemeType>({
+  theme: '',
+  setTheme: () => {}
+});
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function ThemeProvider({children}: Props) {
+  const [theme, setTheme] = usePersistedState('theme', 'light');
+  
+  return (
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
