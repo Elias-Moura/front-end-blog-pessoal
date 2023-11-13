e1ce23507375e95f696f461263ec0840920af563
Elias Moura <elias.smourayt@gmail.com>
2023-11-13 17:03:58 -0300

    Auth provider

--- blog-pessoal/src/main.tsx
+++ blog-pessoal/src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import ThemeProvider from "./contexts/themeInfo.tsx";
+import { AuthProvider } from "./contexts/AuthProvider/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
