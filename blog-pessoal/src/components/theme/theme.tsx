eb80722c0d0738c951111d9637991a70b990debc
Elias Moura <elias.smourayt@gmail.com>
2023-11-16 11:59:56 -0300

    Editar funcionando (U) do CRUD

--- blog-pessoal/src/components/theme/theme.tsx
+++ blog-pessoal/src/components/theme/theme.tsx
@@ -1,36 +1,72 @@
-import { useCreateThemeForm } from "../../contexts/create-theme-form/useCreateThemeForm"
+import { useState } from "react";
+import { useCreateThemeForm } from "../../contexts/create-theme-form/useCreateThemeForm";
+import { updateTheme } from "../../service/theme/themeAPI";
+
 
 interface Props {
-  name: string
-  description: string
+  id: number,
+  titulo: string;
+  descricao: string;
 }
 
-export default function Theme({name, description}: Props) {
-  const {show} = useCreateThemeForm()
+export default function Theme({id, titulo, descricao} : Props) {
+  const { show } = useCreateThemeForm();
+  const [update, setUpdate] = useState(false);
+  const [tema, setTema] = useState<Props>({id: id, titulo: titulo, descricao: descricao })
+
+  async function handleEdit() {
+    console.log("editar", tema) ;
+
+    const response = await updateTheme(tema)
+
+    console.log("Response: ", response)
+  }
+
+  function handleDelete() {
+    console.log("deletei.")
+  }
+
+  function refreshState(e:ChangeEvent<HTMLInputElement>){
+    setTema({...tema, [e.target.name]: e.target.value})
+  }
+
   return (
-<div
+    <div
       className="
         dark:bg-zinc-600
-        rounded-xl my-auto mx-[2rem] px-8
-        flex justify-around text-white bg-primary
+        rounded-xl mx-[2rem] px-8
+        flex text-white bg-primary
         shadow-2xl
         w-[60%]
         lg:w-[80%] lg:max-w-[370px]
         xl:w-[85%] xl:max-w-[370px]
         justify-self-center
+        hover:scale-110 duration-300
         "
     >
-      <div className="flex flex-col content-around justify-center min-h-[450px] w-[90%] gap-[4rem]">
-        <div className="flex flex-col items-center h-[30%]">
-          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-6">
-            {name}
-          </h2>
-          <p className="text-xl wrap">{description}</p>
+      <div className="flex flex-col justify-center min-h-[450px] w-[100%] gap-[4rem]">
+        <div className="flex flex-col  items-center min-h-[40%]">
+            <textarea
+              name="titulo"
+              className="bg-transparent grow text-center text-3xl md:text-4xl font-bold w-full
+              focus:outline-none focus:ring-0 resize-none h-[100%]
+              "
+              value={tema.titulo}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>{refreshState(e)}}
+            />
+            <textarea
+              name="descricao"
+              className="bg-transparent text-xl w-full h-[100%] mt-4
+              focus:outline-none focus:ring-0 resize-none
+              "
+              value={tema.descricao}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>{refreshState(e)}}
+            />
         </div>
         <div className="flex flex-col justify-center gap-4">
           <button
             disabled={show}
-            onClick={() => {console.log('Atualizar tema')}}
+            onClick={handleEdit}
             className="text-xl text-center w-full font-bold 
               border rounded-md duration-500 hover:shadow-2xl 
               hover:shadow-blue-400 hover:border hover:bg-blue-100 
@@ -42,7 +78,9 @@ export default function Theme({name, description}: Props) {
           </button>
           <button
             disabled={show}
-            onClick={() => {console.log('Deletar tema')}}
+            onClick={() => {
+              console.log("Deletar tema");
+            }}
             className="text-xl text-center w-full font-bold 
               border border-transparent rounded-md duration-500 hover:shadow-2xl 
               hover:shadow-red-400 hover:border hover:bg-red-600 
@@ -55,5 +93,5 @@ export default function Theme({name, description}: Props) {
         </div>
       </div>
     </div>
-  )
+  );
 }
--- blog-pessoal/src/service/theme/themeAPI.ts
+++ blog-pessoal/src/service/theme/themeAPI.ts
@@ -1,8 +1,8 @@
 import Api from "../api"
 
-export async function fetchThemes(actives=true, size=4, page=0, sortMethod='id') {
+export async function fetchThemes(active=true, size=4, page=0, sortMethod='id') {
   try {
-    const response = await Api.get(`/temas?page=${page}&size=${size}&sort=${sortMethod}`)
+    const response = await Api.get(`/temas?page=${page}&size=${size}&sort=${sortMethod}&isActive=${active}`)
     return response.data
   } catch (error) {
     console.log(error)
@@ -13,6 +13,16 @@ export async function fetchThemes(actives=true, size=4, page=0, sortMethod='id')
 export async function createTheme(theme: {titulo: string, descricao: string}) {
   try {
     const response = await Api.post("/temas", theme)
+    return response.data
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export async function updateTheme(theme: {id: number, titulo: string, descricao: string}) {
+  try {
+    const response = await Api.put("/temas", theme)
     return response.data
   } catch (error) {
     console.log(error)
