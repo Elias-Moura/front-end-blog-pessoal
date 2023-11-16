9a0521ffedfa0bafa5c71ff1d57856694d4f4e1b
Elias Moura <elias.smourayt@gmail.com>
2023-11-15 22:56:04 -0300

    Navbar c lógica p/ exibir mais botões se logado

--- blog-pessoal/src/components/navbar/Navbar.tsx
+++ blog-pessoal/src/components/navbar/Navbar.tsx
@@ -1,12 +1,25 @@
-import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import sunImg from "./images/sun.svg";
 import moonImg from "./images/moon.svg";
 import { ThemeContext } from "../../contexts/themeInfo";
+import { useAuth } from "../../contexts/AuthProvider/useAuth";
 
 function Navbar() {
   const [loggend, setLogged] = useState(false);
   const { theme, setTheme } = useContext(ThemeContext);
+  
+  const navigate = useNavigate()
+  const auth = useAuth()
+
+  useEffect(() => {
+    if (auth.email) {
+      setLogged(true);
+    }else {
+      setLogged(false);
+    }
+  }, [auth.email])
+
 
   function handleToggleTheme(){
     if ( theme === 'light') {
@@ -14,6 +27,11 @@ function Navbar() {
     } else {
       setTheme('light')
     }
+  }
+
+  function handleLogout() {
+    auth.logout()
+    navigate('/')
   }
 
   return (
@@ -24,36 +42,41 @@ function Navbar() {
     >
       <div className="flex justify-between text-lg ">
         <div className="flex justify-center items-center gap-4 ">
-          <Link
-            to="/login"
-            className="text-xl  p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black "
-          >
-            Entrar
-          </Link>
-          <Link
-            to="/"
-            className="text-xl p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black "
-          >
-            Home
-          </Link>
+          {!loggend ? (
+            <>
+              <Link
+              to="/login"
+              className="text-xl  p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black "
+            >
+              Entrar
+            </Link>
+            <Link
+              to="/"
+              className="text-xl p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black "
+            >
+              Home
+            </Link>
+            </>
+          ): (
+            ""
+          )
+          }
+
 
           {loggend ? (
             <>
-              <a className="text-xl font-bold p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black ">
+              <Link to='/posts' className="text-xl font-bold p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black ">
                 Postagens
-              </a>
-              <a className="text-xl font-bold p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black ">
+              </Link>
+              <Link to='/temas' className="text-xl font-bold p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black ">
                 Temas
-              </a>
-              <a className="text-xl font-bold p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black ">
-                Cadastrar tema
-              </a>
-              <a className="text-xl font-bold p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black ">
+              </Link>
+              <Link to='perfil' className="text-xl font-bold p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black ">
                 Perfil
-              </a>
-              <a className="text-xl font-bold p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black ">
+              </Link>
+              <button onClick={handleLogout} className="text-xl font-bold p-2 border border-transparent rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black ">
                 Sair
-              </a>
+              </button>
             </>
           ) : (
             ""
@@ -68,9 +91,9 @@ function Navbar() {
           />
           <label htmlFor="darkModeCheck" className="border border-transparent p-1 rounded-full duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-zinc-800 hover:text-black">
             {theme === 'light' ? 
-              <img width="35px" src={moonImg} alt="" />
+              <img width="35px" src={moonImg} alt="Botão para trocar o tema entre claro e escuro." />
              : 
-              <img width="35px" src={sunImg} alt="" />
+              <img width="35px" src={sunImg} alt="Botão para trocar o tema entre claro e escuro." />
             }
           </label>
         </div>
