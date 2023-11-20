f4c392c2dca568f3a79bc733708f8f648ef5867d
Elias Moura <elias.smourayt@gmail.com>
2023-11-20 19:32:56 -0300

    formatação

--- blog-pessoal/src/main.tsx
+++ blog-pessoal/src/main.tsx
@@ -4,9 +4,9 @@ import ThemeProvider from "./contexts/themeInfo.tsx";
 import { AuthProvider } from "./contexts/AuthProvider/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
+  <ThemeProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </ThemeProvider>
 );
