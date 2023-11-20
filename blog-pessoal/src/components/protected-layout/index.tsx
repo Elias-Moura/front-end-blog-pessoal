e782dc79ab2aff40ab5f1eaac2bfb6f7d3cec1a9
Elias Moura <elias.smourayt@gmail.com>
2023-11-20 19:35:18 -0300

    Melhorando mensagem quando o usuário não tem acesso ao recurso.

--- blog-pessoal/src/components/protected-layout/index.tsx
+++ blog-pessoal/src/components/protected-layout/index.tsx
@@ -1,10 +1,30 @@
 import { useAuth } from "../../contexts/AuthProvider/useAuth"
+import { Link } from "react-router-dom"
 
 export const ProtectedLayout = ({children}: {children: JSX.Element}) => {
   const auth = useAuth()
 
   if (!auth.email) {
-    return <h1>Você não tem acesso</h1>
+    return (
+    <div className="bg-primary dark:bg-zinc-600  max-w-[30vw] p-8 rounded-md flex flex-col items-center justify-center">
+      <span className="text-3xl text-white mb-10 wrap text-center">Opa, parece que você não está logado.</span>
+
+      <Link
+            to={"/login"}
+            className="text-lg  w-full text-center font-bold p-2 border rounded-md duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black 
+            text-white py-2 px-12 mb-4"
+          >
+            Entrar
+      </Link>
+      <Link
+            to={"/cadastrar"}
+            className="text-lg w-full text-center font-bold p-2 border  rounded-md duration-500 hover:shadow-2xl hover:shadow-blue-400 hover:border hover:bg-blue-100 hover:text-black 
+           text-white py-2 px-12 mb-4"
+          >
+            Cadastre-se
+      </Link>
+    </div>
+    )
   }
   return children
 }
