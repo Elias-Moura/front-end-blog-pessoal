8d4f5c8c1ee365078bac4c680b3cfd6ce33918d3
Elias Moura <elias.smourayt@gmail.com>
2023-11-20 19:34:35 -0300

    deixando mais genérico para reutilizar

--- blog-pessoal/src/components/list-theme/index.tsx
+++ blog-pessoal/src/components/list-theme/index.tsx
@@ -1,5 +1,5 @@
-export default function ListThemes({children,}: {children: JSX.Element[] | JSX.Element;}) {
+export default function ListOfContent({children,}: {children: JSX.Element[] | JSX.Element;}) {
   return <div className="grid grid-cols-1
-  lg:grid-cols-2 xl:grid-cols-3
+  lg:grid-cols-2 xl:grid-cols-4
   items-center justify-center gap-8">{children}</div>;
 }
