173eed8eccfdf319189dd0dbc385aeca5453c468
Elias Moura <elias.smourayt@gmail.com>
2023-11-20 19:31:55 -0300

    Adicionando novas rotas.

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -8,15 +8,18 @@ import MainContent from "./components/main-frame/main-frame";
 import { useContext } from "react";
 import { ThemeContext } from "./contexts/themeInfo";
 import Register from "./pages/register/Register";
-import { ProtectedLayout } from "./components/protected-layout";
 import ThemesPage from "./pages/themes";
-import CreateThemeFormProvider from "./contexts/create-theme-form/themeFormContext";
+import PostsPage from "./pages/posts/posts";
+import { ToastContainer } from "react-toastify";
 
+import 'react-toastify/dist/ReactToastify.css';
+import Theme from "./components/theme/theme";
 
 function App() {
   const { theme } = useContext(ThemeContext);
   return (
     <div className={`min-h-screen flex flex-col ${theme}`}>
+      <ToastContainer/>
       <BrowserRouter>
         <Navbar />
         <MainContent>
@@ -24,9 +27,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/cadastrar" element={<Register />} />
-            <Route path="/posts" element={<ProtectedLayout><h1 className="text-white text-2xl">Você está na sessão de posts!</h1></ProtectedLayout>}></Route>
-            <Route path="/temas" element={<CreateThemeFormProvider><ThemesPage></ThemesPage></CreateThemeFormProvider>}>
-            </Route>
+            <Route path="/posts" element={<PostsPage/>} />
+            <Route path="/temas" element={<ThemesPage/>}/>
+            <Route path="/temas/:id_" element={<Theme id={0} titulo={""} descricao={""} />}/>
           </Routes>
         </MainContent>
         <Footer />
